7a4304f00a05bf0df676de87e093be627fd467d8
JWonderchilds <joshua.childs01@outlook.com>
2023-09-05 16:47:06 +1000

    updated setup.py

--- setup.py
+++ setup.py
@@ -10,9 +10,17 @@ symp_source_files = base_code + symp_code
 
 symp_source_files.append("cython/symp_basis.pyx")
 
+base_head = glob(os.path.join('snappy_src/headers', '*.h'))
+symp_head = glob(os.path.join('symp_src', '*.h'))
+
+symp_header_files = base_head + symp_head
+
 symp_ext = Extension(
     name="symplectic_basis",
     sources=symp_source_files,
+    library_dirs=["symp_src",
+                  "snappy_src/kernel_code",
+                  "snappy_src/headers"],
     include_dirs=["symp_src",
                   "snappy_src/kernel_code",
                   "snappy_src/headers"],
@@ -21,5 +29,6 @@ symp_ext = Extension(
 
 setup(
     name="symplectic-basis",
+    headers=symp_header_files,
     ext_modules=cythonize(symp_ext, compiler_directives={'language_level': "3"})
 )
