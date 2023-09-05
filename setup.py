9082a6dff95dfa8af3686fbc099184724295eb36
JWonderchilds <joshua.childs01@outlook.com>
2023-09-05 15:04:35 +1000

    c file split packaging base

--- setup.py
+++ setup.py
@@ -4,11 +4,9 @@ from distutils.extension import Extension
 from Cython.Build import cythonize
 from setuptools.glob import glob
 
-base_code = glob(os.path.join('snappea_src', 'kernel_code','*.c'))
-unix_code = glob(os.path.join('snappea_src', 'unix_kit','*.c'))
-addl_code = glob(os.path.join('snappea_src', 'addl_code', '*.c'))
+base_code = glob(os.path.join('snappy_src/kernel_code', '*.c'))
 symp_code = glob(os.path.join('symp_src', '*.c'))
-symp_source_files = base_code + unix_code + addl_code + symp_code
+symp_source_files = base_code + symp_code
 
 symp_source_files.append("cython/symp_basis.pyx")
 
@@ -16,11 +14,8 @@ symp_ext = Extension(
     name="symplectic_basis",
     sources=symp_source_files,
     include_dirs=["symp_src",
-                  "snappea_src/addl_code",
-                  "snappea_src/headers",
-                  "snappea_src/kernel_code",
-                  "snappea_src/real_type",
-                  "snappea_src/unix_kit"],
+                  "snappy_src/kernel_code",
+                  "snappy_src/headers"],
     language="c"
 )
 
